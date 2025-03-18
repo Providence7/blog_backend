a35b383f5ced6eee7854d36e733d1dcba47a3865
olayemi <olayemio521@gmail.com>
2025-03-17 22:21:51 -0400

    first commut

---
+++ routes/webhook.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+import bodyParser from "body-parser"
+import { clerkWebHook } from "../controllers/webhook.cont.js"
+const router = express.Router()
+
+router.post("/clerk", bodyParser.raw({ type: 'application/json' }),clerkWebHook )
+
+export default router
