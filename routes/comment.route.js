6991d0cf6b93b8053ad9c98c85cdcef86d4297af
Providence7 <afuyeuthman2018@gmail.com>
2025-03-26 11:23:28 -0400

    ssj

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -6,13 +6,13 @@ import Comment from "../models/comments.model.js"; // Import Comment model
 const router = express.Router()
 router.post("/comments", async (req, res) => {
   try {
-    const { googleId, name, email, comment, postId } = req.body;
+    const { postId, googleId, name, email, comment, photoURL } = req.body;
 
     // Ensure postId (slug) exists in Post collection
     const postExists = await mongoose.model("Post").findOne({ slug: postId });
     if (!postExists) return res.status(404).json({ error: "Post not found" });
 
-    const newComment = new Comment({ googleId, name, email, comment, postId });
+    const newComment = new Comment({ googleId, name, email, comment, postId , photoURL,});
     await newComment.save();
     res.status(201).json({ message: "Comment saved successfully", newComment });
   } catch (error) {
